cbb86540e490a55fee2240cae541db89dea0f439
diogoraposo13 <diogo.raposo@live.com.pt>
2023-10-13 17:39:04 +0100

    Create Form component

---
+++ desafio-smartfit/src/app/components/forms/forms.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent {
+
+  results=[];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(): void{
+    this.formGroup = this.formBuilder.group({
+      hour:'',
+      showClosed:false
+    })
+  }
+
+  onSubmit():void{
+    console.log(this.formGroup.value)
+  }
+
+  onClean():void{
+    this.formGroup.reset();
+  }
+
+}
