b3199fddd3f88f70ec11cfacc75c5d3d273d2dda
diogoraposo13 <diogo.raposo@live.com.pt>
2023-10-17 01:08:54 +0100

    Create footer component

--- desafio-smartfit/src/app/app.module.ts
+++ desafio-smartfit/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CardsListComponent } from './components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { SubtitleComponent } from './components/subtitle/subtitle.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SubtitleComponent } from './components/subtitle/subtitle.component';
     FormsComponent,
     CardsListComponent,
     CardComponent,
-    SubtitleComponent
+    SubtitleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
